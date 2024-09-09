b28f8f7ba572414eda2ed8725cd17aeb71eccfa2
Die-Eule <gisi@bk.ru>
2024-09-09 13:04:13 +0500

    create first route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/first', function () {
+    $a = 1; // переменная 1
+    $b = 2; // переменная 2
+    $c = $a + $b; // сумма
+    // dd($c);
+
+    // return view('first');
+    return view('first', compact('c')); // переменная должна называться точно также, только без доллара и строка
+});
