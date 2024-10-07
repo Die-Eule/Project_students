ffdad8061aac9b3b90f5814fa454cd18bab9cd81
Студенческая <@rbiu.local>
2024-10-07 12:07:22 +0500

    add factory and seeder for Student

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\StudentController;
 
-Route::get('/', [MainController::class, 'show'])->name('home');
+// Route::get('/', [MainController::class, 'show'])->name('home');
 
 
-Route::get('/first',[MainController::class, 'first'] )->name('first');
+// Route::get('/first',[MainController::class, 'first'] )->name('first');
 
 
 Route::get('/students', [StudentController::class, 'index'])->name('students.index');
